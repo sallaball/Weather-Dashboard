2c68cc81556e82fb927b0f1ad9bb360bb79882b8
sallaball <sallaball1206@gmail.com>
2022-07-07 16:52:57 -0500

    working on getting the weather api working

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,41 +58,65 @@ searchForm.addEventListener('submit', function(e) {
     }
 })
 
-// var city;
+searchHistory.addEventListener('click', function(e) {
+    if (e.target.matches('searchList')) {
+        e.preventDefault()
+        searchList.value = e.target.textContent;
+        searchButton.click();
+    }
+})
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-var listEl = document.getElementById("searchData");
-
-var form = document.querySelector(".city-input");
-form.addEventListener("submit", e=> {
-    e.preventDefault();
-    var inputVal = input.value;
-    
-});
-
-// API key
-var APIKey = "011dffec3dd0ab191bb9b1386db7001b";
-var inputVal = input.value;
-
-fetch(queryURL)
-    .then(function(response) {
+function weather(location) {
+    var apiKey = "011dffec3dd0ab191bb9b1386db7001b";
+    var queryURL = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}}&aqi=no`;
+    fetch(queryURL)
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data) {
-        var searchArr = data.response.docs;
-        for(var i = 0; i < searchArr.length; i++) {
-            var listItem = document.createElement("li");
-            listItem.textContent = searchArr[i].description;
-            listEl.appendChild(listItem);
-        }
+    .then(function(weathers) {
+        var longitude = weathers.location.lon;
+        var latitude = weathers.location.lat;
+
+        
     })
-    .catch(() => {
-        msg.textContent = "Please seach for a valid city";
-    });
+
+}
+
+// var city;
+
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+// var listEl = document.getElementById("searchData");
+
+// var form = document.querySelector(".city-input");
+// form.addEventListener("submit", e=> {
+//     e.preventDefault();
+//     var inputVal = input.value;
+    
+// });
+
+// // API key
+// var APIKey = "011dffec3dd0ab191bb9b1386db7001b";
+// var inputVal = input.value;
+
+// fetch(queryURL)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         var searchArr = data.response.docs;
+//         for(var i = 0; i < searchArr.length; i++) {
+//             var listItem = document.createElement("li");
+//             listItem.textContent = searchArr[i].description;
+//             listEl.appendChild(listItem);
+//         }
+//     })
+//     .catch(() => {
+//         msg.textContent = "Please seach for a valid city";
+//     });
 
 
-var city = [];
+// var city = [];
 
 
 
