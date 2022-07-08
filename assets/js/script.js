90d40361607c781a9403990f7f235381566e8adb
sallaball <sallaball1206@gmail.com>
2022-07-08 12:29:12 -0500

    more updates for bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,13 +20,12 @@ var today = moment();
 
 var searchList = [];
 var maxHistory = 10;
-// debugger;
+
 function initLocalStorage(){
     if (localStorage.getItem('storedSearches')){
         searchList = JSON.parse(localStorage.getItem('storedSearches'));
         for (let i = 0; i < searchList.length; i++) {
             createStoragebtn(searchList);
-            console.log("hello");
         }
     }
 }
@@ -81,12 +80,12 @@ function currentWeather(location) {
         var latitude = weather.coord.lat;
         locationName.textContent = weather.name;
 
-        // tdate.textContent = today.format('MM/DD/YYYY');
+        tdate.textContent = today.format('MM/DD/YYYY');
         humidity.textContent = weather.main.humidity;
         wind.textContent = weather.wind.speed;
         temp.textContent = weather.main.temp;
-        // UVColor(weather.current.uv);
-        // currentIcon.src = "https:" + weather.current.condition.icon;
+        UVColor(weather.value);
+        currentIcon.src = "https:" + weather.icon;
         getForecast(latitude, longitude);
     });
     
@@ -113,25 +112,36 @@ function UVColor(index) {
 function getForecast(lat, lon) {
     var apiKey = "76d1b8a660744f79324838626af74619"
     var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=imperial`;
-    forecastUL.innerHTML = '';
+    // forecastUL.innerHTML = '';
     fetch(queryURL)
     .then(function(response) {
         return response.json();
+        // console.log(response);
     })
     .then(function(data) {
         for(var i = 0; i < 10; i++){
             var forecastDay = data.daily[i];
+            console.log(data);
             var newCard = document.createElement('div');
+            // var img = document.createElement('img');
             var newEl = document.createElement('h4');
             newCard.className = "forecastDay dayCard";
-            forecastUL.appendChild(newCard);
+            // forecastUL.appendChild(newCard);
 
-            newEl.textContent = moment.unix(forecastDay.dt).format('ll');
+            newEl.textContent = moment.unix(forecastDay).format('ll');
             newEl.classList = 'text-center'
             newCard.appendChild(newEl);
 
-            newEl = document.createElement('div');
-            newEl.classList="mx-auto bg-white border rounded mb-2";
+            // newEl = document.createElement('div');
+            // newEl.classList="";
+            // newCard.appendChild(newEl);
+
+            // img.classList = ''
+            // img.src = `https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}.png`;
+            // newEl.appendChild(img);
+
+            newEl = document.createElement('p');
+            newEl.textContent = `Temp: ${Math.floor(forecastDay.temp_max)} / ${Math.floor(forecastDay.temp_min)}`;
             newCard.appendChild(newEl);
 
             newEl = document.createElement('p');
@@ -142,9 +152,7 @@ function getForecast(lat, lon) {
             newEl.textContent = `Wind: ${forecastDay.wind_speed} MPH.`
             newCard.appendChild(newEl);
 
-            newEl = document.createElement('p');
-            newEl.textContent = `Temp: ${Math.floor(forecastDay.temp.max)} / ${Math.floor(forecastDay.temp.min)}`;
-            newCard.appendChild(newEl);
+            
 
         }
     });
@@ -156,3 +164,4 @@ initLocalStorage();
 
 
 
+// ${Math.floor(forecastDay.temp.max)} / ${Math.floor(forecastDay.temp.min)}`;
